93e8c82634feac62258f846524c75b491b95ae20
mathewfelixx <math3wkun@gmail.com>
2026-04-30 20:17:52 +0100

    Somewhat nice design for the new messagebox done, code left to do

--- Filmtopia/Filmtopia/frmMessageBox.Designer.vb
+++ Filmtopia/Filmtopia/frmMessageBox.Designer.vb
@@ -22,9 +22,129 @@ Partial Class frmMessageBox
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.btnNo = New System.Windows.Forms.Button()
+        Me.btnYes = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.FlowLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(173, Byte), Integer), CType(CType(20, Byte), Integer), CType(CType(87, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.lblTitle)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(382, 46)
+        Me.Panel1.TabIndex = 0
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Segoe UI", 16.0!, System.Drawing.FontStyle.Bold)
+        Me.lblTitle.ForeColor = System.Drawing.Color.White
+        Me.lblTitle.Location = New System.Drawing.Point(3, 0)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(122, 37)
+        Me.lblTitle.TabIndex = 2
+        Me.lblTitle.Text = "Success!"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(151, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 16)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnNo)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnYes)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnCancel)
+        Me.FlowLayoutPanel1.Controls.Add(Me.btnOK)
+        Me.FlowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(0, 68)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(382, 35)
+        Me.FlowLayoutPanel1.TabIndex = 2
+        '
+        'btnNo
+        '
+        Me.btnNo.Location = New System.Drawing.Point(3, 3)
+        Me.btnNo.Name = "btnNo"
+        Me.btnNo.Size = New System.Drawing.Size(89, 32)
+        Me.btnNo.TabIndex = 0
+        Me.btnNo.Text = "No"
+        Me.btnNo.UseVisualStyleBackColor = True
+        Me.btnNo.Visible = False
+        '
+        'btnYes
+        '
+        Me.btnYes.Location = New System.Drawing.Point(98, 3)
+        Me.btnYes.Name = "btnYes"
+        Me.btnYes.Size = New System.Drawing.Size(89, 32)
+        Me.btnYes.TabIndex = 1
+        Me.btnYes.Text = "Yes"
+        Me.btnYes.UseVisualStyleBackColor = True
+        Me.btnYes.Visible = False
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(193, 3)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(89, 32)
+        Me.btnCancel.TabIndex = 2
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        Me.btnCancel.Visible = False
+        '
+        'btnOK
+        '
+        Me.btnOK.Location = New System.Drawing.Point(288, 3)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(89, 32)
+        Me.btnOK.TabIndex = 3
+        Me.btnOK.Text = "OK"
+        Me.btnOK.UseVisualStyleBackColor = True
+        Me.btnOK.Visible = False
+        '
+        'frmMessageBox
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "frmMessageBox"
+        Me.ClientSize = New System.Drawing.Size(382, 103)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.FlowLayoutPanel1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmMessageBox"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Filmtopia"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.FlowLayoutPanel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents btnNo As Button
+    Friend WithEvents btnYes As Button
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents btnOK As Button
 End Class
