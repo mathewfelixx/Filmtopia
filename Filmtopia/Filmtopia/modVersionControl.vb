1fc8e6acaf05fb5582b2718e94af9426a59a3383
mathewfelixx <math3wkun@gmail.com>
2026-04-30 19:13:38 +0100

    added comments to subroutines in modVersionControl

--- Filmtopia/Filmtopia/modVersionControl.vb
+++ Filmtopia/Filmtopia/modVersionControl.vb
@@ -2,11 +2,13 @@
 
 Module modVersionControl
     Public appversion As String
+    'function that returns the current appversion concat with the string before it
     Public Function GetVersion() As String
         Dim fullstring As String = "Filmtopia Cinema Management System " & appversion
-
         Return fullstring
     End Function
+
+    'subroutine that loads the current appversion from the database
     Public Sub LoadVersion()
         If DbConnect() Then
             Dim query As String = "SELECT Version FROM tblVersionControl"
@@ -22,6 +24,8 @@ Module modVersionControl
             cn.Close()
         End If
     End Sub
+
+    'subroutine that saves the new appversion into the Filmtopia.mdb
     Public Sub SaveVersion()
         If DbConnect() Then
             Dim query As String = "UPDATE tblVersionControl SET Version = @version"
@@ -31,6 +35,9 @@ Module modVersionControl
             cn.Close()
         End If
     End Sub
+
+
+    'subroutine that will contain all the different lblVersions from different forms.
     Public Sub UpdateAllVersionLabels()
         frmLogin.lblVersion.Text = GetVersion()
         frmMainForm.lblVersion.Text = GetVersion()
