61cb35d62eef29327c14104abe3e35bf3c91acb2
mathewfelixx <math3wkun@gmail.com>
2026-07-27 16:34:29 +0100

    improved frm booking search, cancelling is now a transaction, audit log is better, searching now does titles for movies too, and the csv export is kinda fixed

--- Filmtopia/Filmtopia/frmBookingSearch.Designer.vb
+++ Filmtopia/Filmtopia/frmBookingSearch.Designer.vb
@@ -17,132 +17,190 @@ Partial Class frmBookingSearch
 
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.lblSearch = New System.Windows.Forms.Label()
         Me.txtSearch = New System.Windows.Forms.TextBox()
         Me.btnSearch = New System.Windows.Forms.Button()
         Me.btnShowAll = New System.Windows.Forms.Button()
+        Me.lblResultCount = New System.Windows.Forms.Label()
         Me.dgvBookings = New System.Windows.Forms.DataGridView()
+        Me.lblSelectedBooking = New System.Windows.Forms.Label()
         Me.btnCancelBooking = New System.Windows.Forms.Button()
-        Me.lblRegisterTitle = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.lblRegisterScreening = New System.Windows.Forms.Label()
         Me.cboRegisterScreening = New System.Windows.Forms.ComboBox()
         Me.btnLoadRegister = New System.Windows.Forms.Button()
         Me.btnExportRegister = New System.Windows.Forms.Button()
         Me.dgvRegister = New System.Windows.Forms.DataGridView()
         Me.lblVersion = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         CType(Me.dgvBookings, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.dgvRegister, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
+        'lblHeading
+        '
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.lblHeading.Location = New System.Drawing.Point(16, 12)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(300, 25)
+        Me.lblHeading.TabIndex = 0
+        Me.lblHeading.Text = "Find and manage bookings"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblSearch)
+        Me.GroupBox1.Controls.Add(Me.txtSearch)
+        Me.GroupBox1.Controls.Add(Me.btnSearch)
+        Me.GroupBox1.Controls.Add(Me.btnShowAll)
+        Me.GroupBox1.Controls.Add(Me.lblResultCount)
+        Me.GroupBox1.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.GroupBox1.Location = New System.Drawing.Point(16, 48)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(1103, 76)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Find a booking"
+        '
         'lblSearch
         '
         Me.lblSearch.AutoSize = True
-        Me.lblSearch.Location = New System.Drawing.Point(33, 19)
-        Me.lblSearch.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblSearch.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.lblSearch.Location = New System.Drawing.Point(16, 33)
         Me.lblSearch.Name = "lblSearch"
-        Me.lblSearch.Size = New System.Drawing.Size(188, 16)
+        Me.lblSearch.Size = New System.Drawing.Size(200, 17)
         Me.lblSearch.TabIndex = 0
-        Me.lblSearch.Text = "Booking ID or Customer Name"
+        Me.lblSearch.Text = "Booking ID or customer name"
         '
         'txtSearch
         '
-        Me.txtSearch.Location = New System.Drawing.Point(229, 17)
-        Me.txtSearch.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtSearch.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.txtSearch.Location = New System.Drawing.Point(240, 30)
         Me.txtSearch.Name = "txtSearch"
-        Me.txtSearch.Size = New System.Drawing.Size(316, 22)
+        Me.txtSearch.Size = New System.Drawing.Size(300, 25)
         Me.txtSearch.TabIndex = 1
         '
         'btnSearch
         '
-        Me.btnSearch.Location = New System.Drawing.Point(553, 13)
-        Me.btnSearch.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnSearch.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.btnSearch.Location = New System.Drawing.Point(545, 29)
         Me.btnSearch.Name = "btnSearch"
-        Me.btnSearch.Size = New System.Drawing.Size(263, 30)
+        Me.btnSearch.Size = New System.Drawing.Size(110, 30)
         Me.btnSearch.TabIndex = 2
         Me.btnSearch.Text = "Search"
         Me.btnSearch.UseVisualStyleBackColor = True
         '
         'btnShowAll
         '
-        Me.btnShowAll.Location = New System.Drawing.Point(824, 13)
-        Me.btnShowAll.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnShowAll.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.btnShowAll.Location = New System.Drawing.Point(665, 29)
         Me.btnShowAll.Name = "btnShowAll"
-        Me.btnShowAll.Size = New System.Drawing.Size(298, 30)
+        Me.btnShowAll.Size = New System.Drawing.Size(110, 30)
         Me.btnShowAll.TabIndex = 3
-        Me.btnShowAll.Text = "Show All"
+        Me.btnShowAll.Text = "Show all"
         Me.btnShowAll.UseVisualStyleBackColor = True
+        '
+        'lblResultCount
+        '
+        Me.lblResultCount.AutoSize = True
+        Me.lblResultCount.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.lblResultCount.Location = New System.Drawing.Point(795, 35)
+        Me.lblResultCount.Name = "lblResultCount"
+        Me.lblResultCount.Size = New System.Drawing.Size(100, 17)
+        Me.lblResultCount.TabIndex = 4
+        Me.lblResultCount.Text = ""
         '
         'dgvBookings
         '
         Me.dgvBookings.AllowUserToAddRows = False
         Me.dgvBookings.AllowUserToDeleteRows = False
+        Me.dgvBookings.AllowUserToResizeRows = False
+        Me.dgvBookings.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.dgvBookings.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvBookings.Location = New System.Drawing.Point(16, 56)
-        Me.dgvBookings.Margin = New System.Windows.Forms.Padding(4)
+        Me.dgvBookings.Location = New System.Drawing.Point(16, 136)
         Me.dgvBookings.Name = "dgvBookings"
         Me.dgvBookings.ReadOnly = True
-        Me.dgvBookings.RowHeadersWidth = 51
-        Me.dgvBookings.Size = New System.Drawing.Size(1106, 320)
-        Me.dgvBookings.TabIndex = 4
+        Me.dgvBookings.RowHeadersVisible = False
+        Me.dgvBookings.RowTemplate.Height = 30
+        Me.dgvBookings.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgvBookings.Size = New System.Drawing.Size(1103, 300)
+        Me.dgvBookings.TabIndex = 2
+        '
+        'lblSelectedBooking
+        '
+        Me.lblSelectedBooking.AutoSize = True
+        Me.lblSelectedBooking.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.lblSelectedBooking.Location = New System.Drawing.Point(16, 450)
+        Me.lblSelectedBooking.Name = "lblSelectedBooking"
+        Me.lblSelectedBooking.Size = New System.Drawing.Size(300, 17)
+        Me.lblSelectedBooking.TabIndex = 3
+        Me.lblSelectedBooking.Text = "No booking selected"
         '
         'btnCancelBooking
         '
-        Me.btnCancelBooking.Location = New System.Drawing.Point(16, 390)
-        Me.btnCancelBooking.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnCancelBooking.Enabled = False
+        Me.btnCancelBooking.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold)
+        Me.btnCancelBooking.Location = New System.Drawing.Point(949, 442)
         Me.btnCancelBooking.Name = "btnCancelBooking"
-        Me.btnCancelBooking.Size = New System.Drawing.Size(357, 40)
-        Me.btnCancelBooking.TabIndex = 5
-        Me.btnCancelBooking.Text = "Cancel Booking"
+        Me.btnCancelBooking.Size = New System.Drawing.Size(170, 32)
+        Me.btnCancelBooking.TabIndex = 4
+        Me.btnCancelBooking.Text = "Cancel booking"
         Me.btnCancelBooking.UseVisualStyleBackColor = True
         '
-        'lblRegisterTitle
+        'GroupBox2
         '
-        Me.lblRegisterTitle.AutoSize = True
-        Me.lblRegisterTitle.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Bold)
-        Me.lblRegisterTitle.Location = New System.Drawing.Point(16, 444)
-        Me.lblRegisterTitle.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.lblRegisterTitle.Name = "lblRegisterTitle"
-        Me.lblRegisterTitle.Size = New System.Drawing.Size(155, 23)
-        Me.lblRegisterTitle.TabIndex = 7
-        Me.lblRegisterTitle.Text = "Screening Register"
+        Me.GroupBox2.Controls.Add(Me.lblRegisterScreening)
+        Me.GroupBox2.Controls.Add(Me.cboRegisterScreening)
+        Me.GroupBox2.Controls.Add(Me.btnLoadRegister)
+        Me.GroupBox2.Controls.Add(Me.btnExportRegister)
+        Me.GroupBox2.Controls.Add(Me.dgvRegister)
+        Me.GroupBox2.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.GroupBox2.Location = New System.Drawing.Point(16, 486)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(1103, 252)
+        Me.GroupBox2.TabIndex = 5
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Who is coming to a screening"
         '
         'lblRegisterScreening
         '
         Me.lblRegisterScreening.AutoSize = True
-        Me.lblRegisterScreening.Location = New System.Drawing.Point(16, 480)
-        Me.lblRegisterScreening.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblRegisterScreening.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.lblRegisterScreening.Location = New System.Drawing.Point(16, 33)
         Me.lblRegisterScreening.Name = "lblRegisterScreening"
-        Me.lblRegisterScreening.Size = New System.Drawing.Size(68, 16)
-        Me.lblRegisterScreening.TabIndex = 8
+        Me.lblRegisterScreening.Size = New System.Drawing.Size(70, 17)
+        Me.lblRegisterScreening.TabIndex = 0
         Me.lblRegisterScreening.Text = "Screening"
         '
         'cboRegisterScreening
         '
         Me.cboRegisterScreening.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.cboRegisterScreening.FormattingEnabled = True
-        Me.cboRegisterScreening.Location = New System.Drawing.Point(110, 476)
-        Me.cboRegisterScreening.Margin = New System.Windows.Forms.Padding(4)
+        Me.cboRegisterScreening.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.cboRegisterScreening.Location = New System.Drawing.Point(110, 30)
         Me.cboRegisterScreening.Name = "cboRegisterScreening"
-        Me.cboRegisterScreening.Size = New System.Drawing.Size(500, 24)
-        Me.cboRegisterScreening.TabIndex = 9
+        Me.cboRegisterScreening.Size = New System.Drawing.Size(470, 25)
+        Me.cboRegisterScreening.TabIndex = 1
         '
         'btnLoadRegister
         '
-        Me.btnLoadRegister.Location = New System.Drawing.Point(630, 474)
-        Me.btnLoadRegister.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnLoadRegister.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.btnLoadRegister.Location = New System.Drawing.Point(596, 29)
         Me.btnLoadRegister.Name = "btnLoadRegister"
-        Me.btnLoadRegister.Size = New System.Drawing.Size(140, 30)
-        Me.btnLoadRegister.TabIndex = 10
-        Me.btnLoadRegister.Text = "Load Register"
+        Me.btnLoadRegister.Size = New System.Drawing.Size(130, 30)
+        Me.btnLoadRegister.TabIndex = 2
+        Me.btnLoadRegister.Text = "Load register"
         Me.btnLoadRegister.UseVisualStyleBackColor = True
         '
         'btnExportRegister
         '
-        Me.btnExportRegister.Location = New System.Drawing.Point(780, 474)
-        Me.btnExportRegister.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnExportRegister.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.btnExportRegister.Location = New System.Drawing.Point(736, 29)
         Me.btnExportRegister.Name = "btnExportRegister"
-        Me.btnExportRegister.Size = New System.Drawing.Size(140, 30)
-        Me.btnExportRegister.TabIndex = 11
+        Me.btnExportRegister.Size = New System.Drawing.Size(130, 30)
+        Me.btnExportRegister.TabIndex = 3
         Me.btnExportRegister.Text = "Export CSV"
         Me.btnExportRegister.UseVisualStyleBackColor = True
         '
@@ -150,50 +208,50 @@ Partial Class frmBookingSearch
         '
         Me.dgvRegister.AllowUserToAddRows = False
         Me.dgvRegister.AllowUserToDeleteRows = False
+        Me.dgvRegister.AllowUserToResizeRows = False
+        Me.dgvRegister.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.dgvRegister.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvRegister.Location = New System.Drawing.Point(16, 514)
-        Me.dgvRegister.Margin = New System.Windows.Forms.Padding(4)
+        Me.dgvRegister.Location = New System.Drawing.Point(16, 70)
         Me.dgvRegister.Name = "dgvRegister"
         Me.dgvRegister.ReadOnly = True
-        Me.dgvRegister.RowHeadersWidth = 51
-        Me.dgvRegister.Size = New System.Drawing.Size(1106, 180)
-        Me.dgvRegister.TabIndex = 12
+        Me.dgvRegister.RowHeadersVisible = False
+        Me.dgvRegister.RowTemplate.Height = 28
+        Me.dgvRegister.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgvRegister.Size = New System.Drawing.Size(1071, 168)
+        Me.dgvRegister.TabIndex = 4
         '
         'lblVersion
         '
         Me.lblVersion.AutoSize = True
-        Me.lblVersion.ForeColor = System.Drawing.Color.Gray
-        Me.lblVersion.Location = New System.Drawing.Point(16, 706)
-        Me.lblVersion.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblVersion.Font = New System.Drawing.Font("Segoe UI", 8.25!)
+        Me.lblVersion.Location = New System.Drawing.Point(16, 748)
         Me.lblVersion.Name = "lblVersion"
-        Me.lblVersion.Size = New System.Drawing.Size(271, 16)
+        Me.lblVersion.Size = New System.Drawing.Size(240, 13)
         Me.lblVersion.TabIndex = 6
         Me.lblVersion.Text = "Filmtopia Cinema Management System  v1.0"
         '
         'frmBookingSearch
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1135, 730)
+        Me.ClientSize = New System.Drawing.Size(1135, 775)
         Me.Controls.Add(Me.lblVersion)
-        Me.Controls.Add(Me.dgvRegister)
-        Me.Controls.Add(Me.btnExportRegister)
-        Me.Controls.Add(Me.btnLoadRegister)
-        Me.Controls.Add(Me.cboRegisterScreening)
-        Me.Controls.Add(Me.lblRegisterScreening)
-        Me.Controls.Add(Me.lblRegisterTitle)
+        Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.btnCancelBooking)
+        Me.Controls.Add(Me.lblSelectedBooking)
         Me.Controls.Add(Me.dgvBookings)
-        Me.Controls.Add(Me.btnShowAll)
-        Me.Controls.Add(Me.btnSearch)
-        Me.Controls.Add(Me.txtSearch)
-        Me.Controls.Add(Me.lblSearch)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.lblHeading)
         Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "frmBookingSearch"
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
         Me.Text = "Booking Search"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
         CType(Me.dgvBookings, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.dgvRegister, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
@@ -201,13 +259,17 @@ Partial Class frmBookingSearch
 
     End Sub
 
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents lblSearch As Label
     Friend WithEvents txtSearch As TextBox
     Friend WithEvents btnSearch As Button
     Friend WithEvents btnShowAll As Button
+    Friend WithEvents lblResultCount As Label
     Friend WithEvents dgvBookings As DataGridView
+    Friend WithEvents lblSelectedBooking As Label
     Friend WithEvents btnCancelBooking As Button
-    Friend WithEvents lblRegisterTitle As Label
+    Friend WithEvents GroupBox2 As GroupBox
     Friend WithEvents lblRegisterScreening As Label
     Friend WithEvents cboRegisterScreening As ComboBox
     Friend WithEvents btnLoadRegister As Button
--- Filmtopia/Filmtopia/frmBookingSearch.vb
+++ Filmtopia/Filmtopia/frmBookingSearch.vb
@@ -4,6 +4,8 @@ Public Class frmBookingSearch
 
     'the booking currently selected in the grid, 0 means nothing selected
     Private selectedBookingID As Integer = 0
+    'a description of that booking, kept so it can go in the log after the booking is deleted
+    Private selectedBookingText As String = ""
 
     Private Sub frmBookingSearch_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         CommonFormStartup(Me)
@@ -64,8 +66,12 @@ Public Class frmBookingSearch
                 SQLCmd.CommandText = baseQuery & " WHERE tblBooking.BookingID = @SearchID"
                 SQLCmd.Parameters.AddWithValue("@SearchID", CInt(searchText))
             Else
-                SQLCmd.CommandText = baseQuery & " WHERE CustomerForename & ' ' & CustomerSurname LIKE @SearchName"
+                'search the customer name and the film title, so typing a film finds everyone
+                'booked onto it. the two parameters are added in the order they appear in the SQL
+                SQLCmd.CommandText = baseQuery & " WHERE CustomerForename & ' ' & CustomerSurname LIKE @SearchName " &
+                                     "OR FilmTitle LIKE @SearchFilm"
                 SQLCmd.Parameters.AddWithValue("@SearchName", "%" & searchText & "%")
+                SQLCmd.Parameters.AddWithValue("@SearchFilm", "%" & searchText & "%")
             End If
 
             Dim da As New OleDbDataAdapter(SQLCmd)
@@ -76,19 +82,64 @@ Public Class frmBookingSearch
         dgvBookings.DataSource = dt
 
         If dgvBookings.Columns.Count > 0 Then
+            dgvBookings.Columns("BookingID").HeaderText = "Booking"
+            dgvBookings.Columns("CustomerName").HeaderText = "Customer"
+            dgvBookings.Columns("FilmTitle").HeaderText = "Film"
+            dgvBookings.Columns("ScreeningDate").HeaderText = "Date"
+            dgvBookings.Columns("ScreeningTime").HeaderText = "Time"
+            dgvBookings.Columns("TotalCost").HeaderText = "Total"
+
+            dgvBookings.Columns("BookingID").Width = 80
+            dgvBookings.Columns("CustomerName").Width = 200
             dgvBookings.Columns("FilmTitle").AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+            dgvBookings.Columns("ScreeningDate").Width = 110
+            dgvBookings.Columns("ScreeningTime").Width = 80
+            dgvBookings.Columns("TotalCost").Width = 100
+
+            dgvBookings.Columns("ScreeningDate").DefaultCellStyle.Format = "dd/MM/yyyy"
             dgvBookings.Columns("TotalCost").DefaultCellStyle.Format = "C"
+            dgvBookings.ClearSelection()
         End If
 
-        selectedBookingID = 0
+        'say how many were found so an empty grid does not look like something went wrong
+        If dt.Rows.Count = 1 Then
+            lblResultCount.Text = "1 booking found"
+        Else
+            lblResultCount.Text = dt.Rows.Count & " bookings found"
+        End If
+
+        ClearSelectedBooking()
     End Sub
 
-    'remembers which booking was clicked so it can be cancelled
+    'nothing is picked, so there is nothing to cancel
+    Private Sub ClearSelectedBooking()
+        selectedBookingID = 0
+        selectedBookingText = ""
+        lblSelectedBooking.Text = "No booking selected"
+        btnCancelBooking.Enabled = False
+    End Sub
+
+    'pressing enter in the search box searches, rather than having to reach for the button
+    Private Sub txtSearch_KeyDown(sender As Object, e As KeyEventArgs) Handles txtSearch.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            LoadBookings(txtSearch.Text.Trim())
+            e.SuppressKeyPress = True
+        End If
+    End Sub
+
+    'remembers which booking was clicked and says on screen which one it is, so the user can see
+    'exactly what they are about to cancel before they press the button
     Private Sub dgvBookings_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvBookings.CellClick
         If e.RowIndex < 0 Then Exit Sub
 
         Dim row As DataGridViewRow = dgvBookings.Rows(e.RowIndex)
         selectedBookingID = CInt(row.Cells("BookingID").Value)
+
+        selectedBookingText = row.Cells("CustomerName").Value.ToString() & " - " &
+                              row.Cells("FilmTitle").Value.ToString()
+
+        lblSelectedBooking.Text = "Selected: booking " & selectedBookingID & " for " & selectedBookingText
+        btnCancelBooking.Enabled = True
     End Sub
 
     'cancels the selected booking, removing its seats, food order and the booking itself
@@ -102,35 +153,64 @@ Public Class frmBookingSearch
             Exit Sub
         End If
 
+        Dim cancelledID As Integer = selectedBookingID
+        Dim cancelledWhat As String = selectedBookingText
+        Dim seatsFreed As Integer = 0
+        Dim worked As Boolean = False
+
         If DbConnect() Then
-            Dim SQLCmd As New OleDbCommand
-            SQLCmd.Connection = cn
+            'all three deletes are done as one transaction. without this, if something went wrong
+            'partway through, the seats could be freed but the booking left behind, which would
+            'leave the database in a mess. this way either all three happen or none of them do
+            Dim trans As OleDbTransaction = cn.BeginTransaction()
 
-            'remove the seats held for this booking
-            SQLCmd.CommandText = "DELETE FROM tblBookingSeat " &
-                                 "WHERE BookingID = @BookingID"
-            SQLCmd.Parameters.AddWithValue("@BookingID", selectedBookingID)
-            SQLCmd.ExecuteNonQuery()
+            Try
+                Dim SQLCmd As New OleDbCommand
+                SQLCmd.Connection = cn
+                SQLCmd.Transaction = trans
 
-            'remove any food and drink ordered for this booking
-            SQLCmd.CommandText = "DELETE FROM tblOrderItem " &
-                                 "WHERE BookingID = @BookingID"
-            SQLCmd.Parameters.Clear()
-            SQLCmd.Parameters.AddWithValue("@BookingID", selectedBookingID)
-            SQLCmd.ExecuteNonQuery()
+                'remove the seats held for this booking
+                SQLCmd.CommandText = "DELETE FROM tblBookingSeat " &
+                                     "WHERE BookingID = @BookingID"
+                SQLCmd.Parameters.AddWithValue("@BookingID", cancelledID)
+                seatsFreed = SQLCmd.ExecuteNonQuery()
 
-            'finally remove the booking itself
-            SQLCmd.CommandText = "DELETE FROM tblBooking " &
-                                 "WHERE BookingID = @BookingID"
-            SQLCmd.Parameters.Clear()
-            SQLCmd.Parameters.AddWithValue("@BookingID", selectedBookingID)
-            SQLCmd.ExecuteNonQuery()
+                'remove any food and drink ordered for this booking
+                SQLCmd.CommandText = "DELETE FROM tblOrderItem " &
+                                     "WHERE BookingID = @BookingID"
+                SQLCmd.Parameters.Clear()
+                SQLCmd.Parameters.AddWithValue("@BookingID", cancelledID)
+                SQLCmd.ExecuteNonQuery()
+
+                'finally remove the booking itself
+                SQLCmd.CommandText = "DELETE FROM tblBooking " &
+                                     "WHERE BookingID = @BookingID"
+                SQLCmd.Parameters.Clear()
+                SQLCmd.Parameters.AddWithValue("@BookingID", cancelledID)
+
+                'if this is zero the booking had already gone, so nothing should be committed
+                If SQLCmd.ExecuteNonQuery() = 0 Then
+                    trans.Rollback()
+                    MessageBox.Show("That booking no longer exists. It may have already been cancelled.", "Cancel Booking", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                Else
+                    trans.Commit()
+                    worked = True
+                End If
+
+            Catch ex As Exception
+                trans.Rollback()
+                MessageBox.Show("The booking could not be cancelled, so nothing was changed. " & ex.Message, "Cancel Booking", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
 
             cn.Close()
         End If
 
-        WriteLog("BOOKING", "Booking " & selectedBookingID & " cancelled")
-        MessageBox.Show("Booking cancelled")
+        'the log is written after the connection is closed because WriteLog opens it again
+        If worked Then
+            WriteLog("BOOKING", "Booking " & cancelledID & " cancelled (" & cancelledWhat & "), " & seatsFreed & " seat(s) freed")
+            MessageBox.Show("Booking cancelled and " & seatsFreed & " seat(s) put back on sale.", "Cancel Booking", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+
         LoadBookings(txtSearch.Text.Trim())
     End Sub
 
@@ -165,11 +245,30 @@ Public Class frmBookingSearch
         dgvRegister.DataSource = dt
 
         If dgvRegister.Columns.Count > 0 Then
+            dgvRegister.Columns("CustomerName").HeaderText = "Customer"
+            dgvRegister.Columns("SeatNumber").HeaderText = "Seat"
+            dgvRegister.Columns("Tickets").HeaderText = "Tickets on that booking"
             dgvRegister.Columns("CustomerName").AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+            dgvRegister.Columns("SeatNumber").Width = 120
+            dgvRegister.Columns("Tickets").Width = 180
+        End If
+
+        'put the number of seats on the group box heading so it is obvious how busy it is
+        If dt.Rows.Count = 0 Then
+            GroupBox2.Text = "Who is coming to a screening - nobody has booked this one yet"
+        Else
+            GroupBox2.Text = "Who is coming to a screening - " & dt.Rows.Count & " seat(s) booked"
         End If
 
         WriteLog("BOOKING", "Register loaded for ScreeningID " & cboRegisterScreening.SelectedValue.ToString())
     End Sub
+
+    'wraps a value in quotes for the csv file, and doubles up any quotes already in it.
+    'without this a name or film title containing a comma would be split into two columns
+    'when the file was opened in Excel
+    Private Function CsvField(value As String) As String
+        Return """" & value.Replace("""", """""") & """"
+    End Function
 
     'saves the register grid to a csv file so it can be printed or used as a physical register
     Private Sub btnExportRegister_Click(sender As Object, e As EventArgs) Handles btnExportRegister.Click
@@ -190,7 +289,9 @@ Public Class frmBookingSearch
 
             'write each row of the register
             For Each row As DataGridViewRow In dgvRegister.Rows
-                writer.WriteLine(row.Cells("CustomerName").Value.ToString() & "," & row.Cells("SeatNumber").Value.ToString() & "," & row.Cells("Tickets").Value.ToString())
+                writer.WriteLine(CsvField(row.Cells("CustomerName").Value.ToString()) & "," &
+                                 CsvField(row.Cells("SeatNumber").Value.ToString()) & "," &
+                                 CsvField(row.Cells("Tickets").Value.ToString()))
             Next
 
             writer.Close()
