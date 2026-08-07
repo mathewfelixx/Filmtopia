e9321dafd0f03dc0fa3ed73c262f19908af23403
mathewfelixx <math3wkun@gmail.com>
2026-08-07 20:24:50 +0100

    made ChangePassword() work. added validation for change password, fixed writelogs bug

--- Filmtopia/Filmtopia/frmSettings.Designer.vb
+++ Filmtopia/Filmtopia/frmSettings.Designer.vb
@@ -22,10 +22,20 @@ Partial Class frmSettings
         Me.btnChooseFolder = New System.Windows.Forms.Button()
         Me.btnCreateBackup = New System.Windows.Forms.Button()
         Me.lblHelp = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.lblCurrent = New System.Windows.Forms.Label()
+        Me.txtCurrentPW = New System.Windows.Forms.TextBox()
+        Me.lblNew = New System.Windows.Forms.Label()
+        Me.txtNewPW = New System.Windows.Forms.TextBox()
+        Me.lblConfirm = New System.Windows.Forms.Label()
+        Me.txtConfirmPW = New System.Windows.Forms.TextBox()
+        Me.btnChangePassword = New System.Windows.Forms.Button()
+        Me.lblPWHelp = New System.Windows.Forms.Label()
         Me.lblVersion = New System.Windows.Forms.Label()
         Me.btnClose = New System.Windows.Forms.Button()
         Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
         Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'lblHeading
@@ -97,22 +107,109 @@ Partial Class frmSettings
         Me.lblHelp.TabIndex = 3
         Me.lblHelp.Text = "Choose a folder first, then create a backup of the database."
         '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.lblCurrent)
+        Me.GroupBox2.Controls.Add(Me.txtCurrentPW)
+        Me.GroupBox2.Controls.Add(Me.lblNew)
+        Me.GroupBox2.Controls.Add(Me.txtNewPW)
+        Me.GroupBox2.Controls.Add(Me.lblConfirm)
+        Me.GroupBox2.Controls.Add(Me.txtConfirmPW)
+        Me.GroupBox2.Controls.Add(Me.btnChangePassword)
+        Me.GroupBox2.Controls.Add(Me.lblPWHelp)
+        Me.GroupBox2.Location = New System.Drawing.Point(16, 210)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(516, 175)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Change Password"
+        '
+        'lblCurrent
+        '
+        Me.lblCurrent.AutoSize = True
+        Me.lblCurrent.Location = New System.Drawing.Point(12, 31)
+        Me.lblCurrent.Name = "lblCurrent"
+        Me.lblCurrent.Size = New System.Drawing.Size(94, 13)
+        Me.lblCurrent.TabIndex = 0
+        Me.lblCurrent.Text = "Current password:"
+        '
+        'txtCurrentPW
+        '
+        Me.txtCurrentPW.Location = New System.Drawing.Point(152, 28)
+        Me.txtCurrentPW.Name = "txtCurrentPW"
+        Me.txtCurrentPW.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtCurrentPW.Size = New System.Drawing.Size(200, 20)
+        Me.txtCurrentPW.TabIndex = 1
+        '
+        'lblNew
+        '
+        Me.lblNew.AutoSize = True
+        Me.lblNew.Location = New System.Drawing.Point(12, 61)
+        Me.lblNew.Name = "lblNew"
+        Me.lblNew.Size = New System.Drawing.Size(81, 13)
+        Me.lblNew.TabIndex = 2
+        Me.lblNew.Text = "New password:"
+        '
+        'txtNewPW
+        '
+        Me.txtNewPW.Location = New System.Drawing.Point(152, 58)
+        Me.txtNewPW.Name = "txtNewPW"
+        Me.txtNewPW.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtNewPW.Size = New System.Drawing.Size(200, 20)
+        Me.txtNewPW.TabIndex = 3
+        '
+        'lblConfirm
+        '
+        Me.lblConfirm.AutoSize = True
+        Me.lblConfirm.Location = New System.Drawing.Point(12, 91)
+        Me.lblConfirm.Name = "lblConfirm"
+        Me.lblConfirm.Size = New System.Drawing.Size(122, 13)
+        Me.lblConfirm.TabIndex = 4
+        Me.lblConfirm.Text = "Confirm new password:"
+        '
+        'txtConfirmPW
+        '
+        Me.txtConfirmPW.Location = New System.Drawing.Point(152, 88)
+        Me.txtConfirmPW.Name = "txtConfirmPW"
+        Me.txtConfirmPW.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtConfirmPW.Size = New System.Drawing.Size(200, 20)
+        Me.txtConfirmPW.TabIndex = 5
+        '
+        'btnChangePassword
+        '
+        Me.btnChangePassword.Location = New System.Drawing.Point(152, 122)
+        Me.btnChangePassword.Name = "btnChangePassword"
+        Me.btnChangePassword.Size = New System.Drawing.Size(140, 30)
+        Me.btnChangePassword.TabIndex = 6
+        Me.btnChangePassword.Text = "Change Password"
+        Me.btnChangePassword.UseVisualStyleBackColor = True
+        '
+        'lblPWHelp
+        '
+        Me.lblPWHelp.AutoSize = True
+        Me.lblPWHelp.ForeColor = System.Drawing.Color.Gray
+        Me.lblPWHelp.Location = New System.Drawing.Point(358, 31)
+        Me.lblPWHelp.Name = "lblPWHelp"
+        Me.lblPWHelp.Size = New System.Drawing.Size(140, 13)
+        Me.lblPWHelp.TabIndex = 7
+        Me.lblPWHelp.Text = "At least 6 characters."
+        '
         'lblVersion
         '
         Me.lblVersion.AutoSize = True
         Me.lblVersion.ForeColor = System.Drawing.Color.Gray
-        Me.lblVersion.Location = New System.Drawing.Point(13, 215)
+        Me.lblVersion.Location = New System.Drawing.Point(13, 400)
         Me.lblVersion.Name = "lblVersion"
         Me.lblVersion.Size = New System.Drawing.Size(45, 13)
-        Me.lblVersion.TabIndex = 2
+        Me.lblVersion.TabIndex = 3
         Me.lblVersion.Text = "lblVersion"
         '
         'btnClose
         '
-        Me.btnClose.Location = New System.Drawing.Point(444, 210)
+        Me.btnClose.Location = New System.Drawing.Point(444, 395)
         Me.btnClose.Name = "btnClose"
         Me.btnClose.Size = New System.Drawing.Size(88, 26)
-        Me.btnClose.TabIndex = 3
+        Me.btnClose.TabIndex = 4
         Me.btnClose.Text = "Close"
         Me.btnClose.UseVisualStyleBackColor = True
         '
@@ -120,9 +217,10 @@ Partial Class frmSettings
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(548, 250)
+        Me.ClientSize = New System.Drawing.Size(548, 435)
         Me.Controls.Add(Me.btnClose)
         Me.Controls.Add(Me.lblVersion)
+        Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.lblHeading)
         Me.Name = "frmSettings"
@@ -132,6 +230,8 @@ Partial Class frmSettings
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -143,6 +243,15 @@ Partial Class frmSettings
     Friend WithEvents btnChooseFolder As Button
     Friend WithEvents btnCreateBackup As Button
     Friend WithEvents lblHelp As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents lblCurrent As Label
+    Friend WithEvents txtCurrentPW As TextBox
+    Friend WithEvents lblNew As Label
+    Friend WithEvents txtNewPW As TextBox
+    Friend WithEvents lblConfirm As Label
+    Friend WithEvents txtConfirmPW As TextBox
+    Friend WithEvents btnChangePassword As Button
+    Friend WithEvents lblPWHelp As Label
     Friend WithEvents lblVersion As Label
     Friend WithEvents btnClose As Button
     Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
--- Filmtopia/Filmtopia/modSettings.vb
+++ Filmtopia/Filmtopia/modSettings.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.IO
+Imports System.Data.OleDb
 
 Module modSettings
     Public Sub DarkMode()
@@ -10,9 +11,57 @@ Module modSettings
     Public Sub ColourScheme()
 
     End Sub
-    Public Sub ChangePassword(Username As String, OldPassword As String)
+    'checks the old password is right and if it is saves the new one, returns True if it worked
+    Public Function ChangePassword(Username As String, OldPassword As String, NewPassword As String) As Boolean
+        Dim changed As Boolean = False
+        Dim wrongPassword As Boolean = False
 
-    End Sub
+        If DbConnect() Then
+            'get the password currently stored for this user
+            Dim SQLCmd As New OleDbCommand
+            SQLCmd.Connection = cn
+            SQLCmd.CommandText = "SELECT Password " &
+                                 "FROM tblLogin " &
+                                 "WHERE Username = @Username"
+            SQLCmd.Parameters.AddWithValue("@Username", Username)
+
+            Dim storedPW As String = ""
+            Dim rs As OleDbDataReader = SQLCmd.ExecuteReader()
+            If rs.Read() Then
+                storedPW = Decrypt(rs("Password"))
+            End If
+            rs.Close()
+
+            If storedPW = "" Then
+                MessageBox.Show("Could not find that user account.", "Change Password", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            ElseIf storedPW <> OldPassword Then
+                MessageBox.Show("Your current password is not correct.", "Change Password", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                wrongPassword = True
+            Else
+                'old password matched so save the new one encrypted
+                Dim SQLCmd2 As New OleDbCommand
+                SQLCmd2.Connection = cn
+                SQLCmd2.CommandText = "UPDATE tblLogin " &
+                                      "SET Password = @Password " &
+                                      "WHERE Username = @Username"
+                SQLCmd2.Parameters.AddWithValue("@Password", Encrypt(NewPassword))
+                SQLCmd2.Parameters.AddWithValue("@Username", Username)
+                SQLCmd2.ExecuteNonQuery()
+                changed = True
+            End If
+
+            cn.Close()
+        End If
+
+        'the log has to be written after the connection is closed because WriteLog opens it again
+        If changed Then
+            WriteLog("AUTH", "User '" & Username & "' changed their password")
+        ElseIf wrongPassword Then
+            WriteLog("AUTH", "User '" & Username & "' failed to change password, wrong current password")
+        End If
+
+        Return changed
+    End Function
 
     'copies Filmtopia.mdb into the folder the user picked, with the date and time in the file name
     'returns the full path it saved to, or an empty string if it did not work
