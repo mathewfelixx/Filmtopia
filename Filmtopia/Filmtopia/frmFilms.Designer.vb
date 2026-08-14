3eebab615f0e51f55fee2923a3e30fe8f708affc
mathewfelixx <math3wkun@gmail.com>
2026-08-15 00:39:18 +0100

    same on frmFilms, had to sit it right of the version label, the checker had them 2px into each other

--- Filmtopia/Filmtopia/frmFilms.Designer.vb
+++ Filmtopia/Filmtopia/frmFilms.Designer.vb
@@ -46,6 +46,7 @@ Partial Class frmFilms
         Me.btnDelete = New System.Windows.Forms.Button()
         Me.btnClear = New System.Windows.Forms.Button()
         Me.btnExport = New System.Windows.Forms.Button()
+        Me.lblSaved = New System.Windows.Forms.Label()
         Me.lblVersion = New System.Windows.Forms.Label()
         Me.GroupBox1.SuspendLayout()
         CType(Me.dgvFilms, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -358,6 +359,14 @@ Partial Class frmFilms
         Me.btnExport.Text = "Export to CSV"
         Me.btnExport.UseVisualStyleBackColor = True
         '
+        'lblSaved
+        '
+        Me.lblSaved.AutoSize = False
+        Me.lblSaved.Location = New System.Drawing.Point(360, 684)
+        Me.lblSaved.Name = "lblSaved"
+        Me.lblSaved.Size = New System.Drawing.Size(692, 16)
+        Me.lblSaved.Text = ""
+        '
         'lblVersion
         '
         Me.lblVersion.AutoSize = True
@@ -374,6 +383,7 @@ Partial Class frmFilms
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1068, 710)
         Me.Controls.Add(Me.btnExport)
+        Me.Controls.Add(Me.lblSaved)
         Me.Controls.Add(Me.lblVersion)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
@@ -423,5 +433,6 @@ Partial Class frmFilms
     Friend WithEvents btnDelete As Button
     Friend WithEvents btnClear As Button
     Friend WithEvents btnExport As Button
+    Friend WithEvents lblSaved As Label
     Friend WithEvents lblVersion As Label
 End Class
